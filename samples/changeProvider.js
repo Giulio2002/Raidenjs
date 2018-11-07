aab7490268e01d82bc456e18afb3af4cb58fb199
Giulio <giulio.rebuffo@gmail.com>
2018-11-07 20:54:29 +0100

    added openChannel method

--- samples/changeProvider.js
+++ samples/changeProvider.js
@@ -2,4 +2,4 @@ const Raiden = require('../index.js')
 
 let raiden = new Raiden("http://localhost:5001")
 raiden.setProvider("http://localhost:9090")
-console.log(raiden.url());
+console.log(raiden.url);
