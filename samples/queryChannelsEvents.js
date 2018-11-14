1ddc61594ad2bd00d3169d7a002ea0faae0cb313
Giulio <giulio.rebuffo@gmail.com>
2018-11-14 20:49:39 +0100

    Added channel queries interactions

--- samples/queryChannelsEvents.js
+++ samples/queryChannelsEvents.js
@@ -2,5 +2,5 @@ const Raiden = require('../index.js')
 
 let raiden = new Raiden("http://localhost:5001")
 // You can also specify a partner in the second argument if you want a more accurate research
-let events = raiden.getTokenNetworkEvents("0xc778417E063141139Fce010982780140Aa0cD5Ab")
+let events = raiden.getChannelsEvents("0xc778417E063141139Fce010982780140Aa0cD5Ab")
 console.log(events);
--- samples/queryTokenNetworksCreationEvents.js
+++ samples/queryTokenNetworksCreationEvents.js
@@ -1,5 +1,5 @@
 const Raiden = require('../index.js')
 
 let raiden = new Raiden("http://localhost:5001")
-let channel = raiden.getTokenNetworksCreationEvents()
-console.log(channel);
+let events = raiden.getTokenNetworksCreationEvents()
+console.log(events);
