762f7f14811d7266e99becbcd8d0f81ac6c256ed
Giulio <giulio.rebuffo@gmail.com>
2018-11-14 20:34:34 +0100

    removed async version of queries for blockchain token

--- samples/queryTokenNetworkEventsAsync.js
+++
@@ -1,7 +0,0 @@
-const Raiden = require('../index.js')
-
-let raiden = new Raiden("http://localhost:5001")
-raiden.getTokenNetworkEventsAsync("0xc778417E063141139Fce010982780140Aa0cD5Ab", (value, error) => {
-	if(error) throw error
-	console.log(value)
-})
